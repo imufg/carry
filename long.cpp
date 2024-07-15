50dd371a05442de9798f055a24736881a680a056
imufg <doe8sxn@outlook.com>
2024-07-15 12:44:54 +0800

    Modify long.cpp

--- long.cpp
+++ long.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Woman beat for administration.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
