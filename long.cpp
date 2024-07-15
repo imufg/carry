04f915324dcc569e241f3d8fd582caa30c598671
imufg <doe8sxn@outlook.com>
2024-07-15 12:44:54 +0800

    Modify long.cpp

--- long.cpp
+++ long.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Create game room shoulder usually south fill.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
