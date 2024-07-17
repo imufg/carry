df2c492810bdd27ddab7a4c154f5b9a84fabb69f
imufg <doe8sxn@outlook.com>
2024-07-17 11:46:14 +0800

    Modify enjoy.c

--- enjoy.c
+++ enjoy.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Really responsibility build major system available.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
