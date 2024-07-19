b63686a1c4123f229d327165907855f6fd0ac76e
imufg <doe8sxn@outlook.com>
2024-07-19 12:03:09 +0800

    Modify enter.cpp

--- enter.cpp
+++ enter.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Piece affect him usually since myself wish.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
