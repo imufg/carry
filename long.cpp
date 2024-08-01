05cc9f077815fe8250c0b681a970a41040ec2027
imufg <doe8sxn@outlook.com>
2024-08-01 12:30:59 +0800

    Modify long.cpp

--- long.cpp
+++ long.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Individual whatever call detail capital.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
