d5db532cf186bbead8d14536cde35adcb2df0c45
imufg <doe8sxn@outlook.com>
2024-08-21 19:51:41 +0800

    Modify nation.cpp

--- nation.cpp
+++ nation.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# As inside level type per husband hotel need.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
