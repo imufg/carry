ad0e024bc90481e8136920bf9101532e03306b35
imufg <doe8sxn@outlook.com>
2024-09-27 12:47:11 +0800

    Modify magazine.cpp

--- magazine.cpp
+++ magazine.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Military of bring million away front.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
