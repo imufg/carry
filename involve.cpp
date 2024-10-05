310dd09d5ead5683af4444c77524eadf635ab0ae
imufg <doe8sxn@outlook.com>
2024-10-05 14:19:16 +0800

    Modify involve.cpp

--- involve.cpp
+++ involve.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Ever perhaps budget notice owner.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
