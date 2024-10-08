544ed9e4c13e437d254556424f0a94418ead41d5
imufg <doe8sxn@outlook.com>
2024-10-08 17:38:22 +0800

    Modify two.cpp

--- two.cpp
+++ two.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# East player garden in.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
