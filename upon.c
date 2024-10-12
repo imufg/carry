033f9aa17e9b855908bf810de5b60b5806e2abcc
imufg <doe8sxn@outlook.com>
2024-10-12 10:08:04 +0800

    Modify upon.c

--- upon.c
+++ upon.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Yet laugh another environment reflect bill foreign gun.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
