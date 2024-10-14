1ca800057bf73084f81eed7f5021e5760c529c56
imufg <doe8sxn@outlook.com>
2024-10-14 10:32:21 +0800

    Modify two.cpp

--- two.cpp
+++ two.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Without process herself those now opportunity.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
