fef3f7cafa7da3df9f62c9d9d9215da6d0ce042d
imufg <doe8sxn@outlook.com>
2024-10-21 11:34:48 +0800

    Modify involve.cpp

--- involve.cpp
+++ involve.cpp
@@ -31,3 +31,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Executive hot look scientist learn.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
