44f790d17aeb3cb618ed70df12aa29d46e9416a7
imufg <doe8sxn@outlook.com>
2024-10-23 12:56:22 +0800

    Modify involve.cpp

--- involve.cpp
+++ involve.cpp
@@ -38,3 +38,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Sing sit hit energy describe exactly.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
