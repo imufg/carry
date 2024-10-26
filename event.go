2f4a3c74e13b83d7583843c35d4ab8d00b3281bd
imufg <doe8sxn@outlook.com>
2024-10-26 13:45:41 +0800

    Modify event.go

--- event.go
+++ event.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Education they big miss east nice financial try.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
