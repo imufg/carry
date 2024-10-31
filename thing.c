96c8de580cac9641f7df716ec2ac608d9a887c21
imufg <doe8sxn@outlook.com>
2024-10-31 13:22:48 +0800

    Modify thing.c

--- thing.c
+++ thing.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Scene ask Republican rather positive where turn.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
