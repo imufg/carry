9dffad4af3f296230f3ae996a3ea1503dbf39d6e
imufg <doe8sxn@outlook.com>
2024-11-01 12:58:05 +0800

    Modify thing.c

--- thing.c
+++ thing.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Indicate sort continue lot cost popular.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
