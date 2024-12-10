f92886995336209a0138989104cf8683dec99721
imufg <doe8sxn@outlook.com>
2024-12-11 06:30:14 +0800

    Modify process.cpp

--- process.cpp
+++ process.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Either employee organization majority least at person.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
