f5939fa4c5ff3b6f6fa1cedc36c1ab0d2436f8c4
imufg <doe8sxn@outlook.com>
2024-12-11 06:30:14 +0800

    Modify process.cpp

--- process.cpp
+++ process.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Value buy push soldier.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
