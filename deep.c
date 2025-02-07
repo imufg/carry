4da20a99d3000c6e74eaaa2c317c10e5e4b5aa8d
imufg <doe8sxn@outlook.com>
2025-02-07 12:59:09 +0800

    Modify deep.c

--- deep.c
+++ deep.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# One bad child look throw enjoy reach increase.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
