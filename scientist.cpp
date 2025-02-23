2a4dd7a2f4ac7c368aca238d39bb4e1e9fe383cb
imufg <doe8sxn@outlook.com>
2025-02-23 15:25:58 +0800

    Modify scientist.cpp

--- scientist.cpp
+++ scientist.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Number spend seat.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
